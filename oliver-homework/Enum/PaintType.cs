2c39e74e8bbc8cd16ae08aafb312f0cdf094c557
oknoporblem110 <oliveryang94@gmail.com>
2026-07-26 12:59:08 +1000

    Add models

---
+++ oliver-homework/Enum/PaintType.cs
@@ -0,0 +1,12 @@
+namespace oliver_homework.Enum;
+
+public enum PaintType
+{
+    Interior = 1,
+    Exterior = 2,
+    Ceiling = 3,
+    Trim = 4,
+    Roof = 5,
+    Floor = 6,
+    Waterproof = 7 
+}
